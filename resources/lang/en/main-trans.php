99b503f7e22fb5dd534c44a21a51bf8fb5e8280f
kelnaazshiekho97 <julianath7896@gmail.com>
2021-06-15 15:12:24 +0300

    main sidebar Localization

--- resources/lang/en/main-trans.php
+++ resources/lang/en/main-trans.php
@@ -1,7 +1,26 @@
 <?php
 
 return [
- 'Dashboard'=>"Dashboard",
- 'Grades'=>"Grades",
- 'GradesList'=>"Grades List",
+    'Dashboard'=>'Dashboard',
+    'Dashboard_page'=>'Dashboard',
+    'Main_title' => 'MoraSoft School Management Program',
+    'Programname' => 'MoraSoft School Management Program',
+    'change_language'=>'language',
+    'Grades'=>'Grades',
+    'GradesList'=>'Grades List',
+    'classes'=>'Classes',
+    'sections'=>'Sections',
+    'students'=>'Students',
+    'Teachers'=>'Teachers',
+    'Parents'=>'Parents',
+    'Accounts'=>'Accounts',
+    'Attendance'=>'Attendance',
+    'Exams'=>'Exams',
+    'library'=>'Library',
+    'Onlineclasses'=>'Online classes',
+    'Settings'=>'Settings',
+    'Users'=>'Users',
+     'Copyright' => 'Copyright',
+     'Name_Programer' => 'SamirGamal MoraSoft All Rights Reserved',
+
 ];
