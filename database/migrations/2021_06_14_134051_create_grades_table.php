3bcaaba1136f0301910299cd678f3494506548c4
Kinda-halabi <kyndahlby@gmail.com>
2021-06-15 22:05:20 +0300

    update grade table.

--- database/migrations/2021_06_14_134051_create_grades_table.php
+++ database/migrations/2021_06_14_134051_create_grades_table.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateGradesTable extends Migration {
 
 	public function up()
 	{
 		Schema::create('Grades', function(Blueprint $table) {
-			$table->increments('id');
+			$table->id();
 			$table->timestamps();
 			$table->string('name')->unique();
     	$table->text('notes')->nullable();
