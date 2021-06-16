1991e240611433dad5f5bf529036edb72e267911
Kinda-halabi <kyndahlby@gmail.com>
2021-06-16 15:39:22 +0300

    add ont to many relation.

--- app/Models/Classroom.php
+++ app/Models/Classroom.php
@@ -4,13 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class Classroom extends Model
 {
     use HasFactory;
+    use HasTranslations;
+    public $translatable = ['name_class'];
+
+    protected $fillable=['name_class','grade_id'];
+    protected $table = 'Classrooms';
+    public $timestamps = true;
 
     public function Grades()
     {
-        return $this->belongsTo('Grade', 'grade_id');
+        return $this->belongsTo(Grade::class,'grade_id');
+
     }
 }
