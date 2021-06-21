91de2b9e8f04b4e8936c3464f0db0c4b0e4b9392
Kinda-halabi <kyndahlby@gmail.com>
2021-06-21 23:41:59 +0300

    update teachers model.

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -4,19 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class Teacher extends Model
 {
     use HasFactory;
-    public function Specializations()
-    {
-        return $this->belongsTo(Specialization::class,'Specialization_id');
+    use HasTranslations;
 
+    public $translatable = ['Name'];
+    protected $guarded=[];
+
+    // Relation one to many with specialization
+    public function specializations()
+    {
+        return $this->belongsTo('App\Models\Specialization', 'Specialization_id');
     }
 
-    public function Genders()
+    // Relation one to many with gender
+    public function genders()
     {
-        return $this->belongsTo(Gender::class,'Gender_id');
-
+        return $this->belongsTo('App\Models\Gender', 'Gender_id');
     }
+
+
 }
+
