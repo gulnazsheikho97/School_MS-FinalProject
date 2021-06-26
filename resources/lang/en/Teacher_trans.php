f531f68bd675f7337e3376d2f3bf4de684031cff
kelnaazshiekho97 <julianath7896@gmail.com>
2021-06-26 18:02:11 +0300

    correct translation

--- resources/lang/en/Teacher_trans.php
+++ resources/lang/en/Teacher_trans.php
@@ -7,11 +7,11 @@ return [
     'Delete_Teacher'=>'Delete Teacher',
     'Email'=>'Email',
     'Password'=>'Password',
-    'Name_ar'=>'Name_ar',
-    'Name_en'=>'Name_en',
+    'Name_ar'=>'Name in Arabic',
+    'Name_en'=>'Name in English',
     'specialization'=>'specialization',
     'Gender'=>'Gender',
-    'Joining_Date'=>'Joining_Date',
+    'Joining_Date'=>'Joining Date',
     'Address'=>'Address',
     'Next' => 'Next',
     'Back' => 'Back',
@@ -21,5 +21,5 @@ return [
     'Processes' => 'Processes',
     'Close' => 'Close',
     'submit' => 'Submit',
-    'Warning_Grade'=>'Are you sure you want to delete this Teacher'
+    'Warning_Grade'=>'Are you sure you want to delete this Teacher ?',
 ];
