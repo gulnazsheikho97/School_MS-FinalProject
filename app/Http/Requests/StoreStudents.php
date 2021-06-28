6dbd1542077d79edb57813b1d090c328eaa64b54
kelnaazshiekho97 <julianath7896@gmail.com>
2021-06-28 22:15:43 +0300

    student model

---
+++ app/Http/Requests/StoreStudents.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreStudents extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'student_name_ar' => 'required',
+            'student_name_en' => 'required',
+            'email' => 'required|email|unique:students,email',
+            'password' => 'required|string|min:6|max:10',
+            'gender_id' => 'required',
+            'nationalitie_id' => 'required',
+            'blood_id' => 'required',
+            'Date_Birth' => 'required|date|date_format:Y-m-d',
+            'Grade_id' => 'required',
+            'Classroom_id' => 'required',
+            'section_id' => 'required',
+           // 'parent_id' => 'required',
+            'academic_year' => 'required',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'email.required' => trans('validation.required'),
+            'email.unique' => trans('validation.unique'),
+            'Password.required' => trans('validation.required'),
+            'student_name_ar.required' => trans('validation.required'),
+            'student_name_en.required' => trans('validation.required'),
+            'blood_id.required' => trans('validation.required'),
+            'gender_id.required' => trans('validation.required'),
+            'nationalitie_id.required' => trans('validation.required'),
+            'Date_Birth.required' => trans('validation.required'),
+            'Classroom_id.required' => trans('validation.required'),
+            'section_id.required' => trans('validation.required'),
+            //'parent_id.required' => trans('validation.required'),
+            'academic_year.required' => trans('validation.required'),
+        ];
+    }
+}
