3b52b6e31e0ee432427732d0aca8779ba3cf7815
Kinda-halabi <kyndahlby@gmail.com>
2021-06-29 14:34:32 +0300

    use spatie package in parent model .

--- app/Models/My_Parent.php
+++ app/Models/My_Parent.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class My_Parent extends Model
 {
     use HasFactory;
+    use HasTranslations;//use spatie package for translate column
+    public $translatable = ['Name_Father','Job_Father','Name_Mother','Job_Mother'];//the field i want to translate
+    protected $guarded=[];//accept all field from the form(insted on $fillable)
 }
