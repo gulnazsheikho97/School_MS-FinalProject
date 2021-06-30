aa5d39b189dae4016ab3a88b3f248916f6cf3194
kelnaazshiekho97 <julianath7896@gmail.com>
2021-06-30 17:31:23 +0300

    make students table relashionsheps

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -12,4 +12,28 @@ class Student extends Model
     use HasTranslations;
     public $translatable = ['student_name'];
     protected $guarded =[];
+
+    // Relation one to many with gender
+    public function genders()
+    {
+        return $this->belongsTo('App\Models\Gender', 'gender_id');
+    }
+
+    // Relation one to many with grades
+    public function grade()
+    {
+        return $this->belongsTo('App\Models\Grade', 'Grade_id');
+    }
+
+    // Relation one to many with classes
+    public function classroom()
+    {
+        return $this->belongsTo('App\Models\Classroom', 'Classroom_id');
+    }
+
+    // Relation one to many with  sections
+    public function section()
+    {
+        return $this->belongsTo('App\Models\Section', 'section_id');
+    }
 }
